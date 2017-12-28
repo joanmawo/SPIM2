9374c9ea0fdd38266a2dbaf66203800cf29d1327
Jorge Madrid <ja.madrid2714@uniandes.edu.co>
2017-12-27 23:40:19 -0500

    initial weights updated

--- curve_fit2.m
+++ curve_fit2.m
@@ -1,52 +1,62 @@
 
-F = @(x,Z_1_70_n)x(1)*(2*besselj(1,x(2)*(Z_1_70_n-x(3)))./(x(2)*(Z_1_70_n-x(3)))).^2 + x(4)*(2*besselj(1,x(5)*(Z_1_70_n-x(6)))./(x(5)*((Z_1_70_n-x(6))))).^2 + x(7);
+F = @(x,Z_1_57_extended)x(1)*(2*besselj(1,x(2)*(Z_1_57_extended-x(3)))./(x(2)*(Z_1_57_extended-x(3)))).^2 + x(4)*(2*besselj(1,x(5)*(Z_1_57_extended-x(6)))./(x(5)*((Z_1_57_extended-x(6))))).^2 + x(7);
 
 %xo = [10;0.2;14;5;0.1;35]; xo = [0.1;0.1366;0.3978;0.5484;0.01;42.273;3];[200;15;14.5;70;47;32.5;4];
 
 options = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt');
-xo = [94;0.25;13.2;100;5.11;31.5;42.0619];
-lb = [50;0.1;8;20;0.1;30;0];
-ub = [220;100;16;1000;2.5;36;50];
+% % ALPHA
+% xo = [130;16;0.7;25;24;1.45;55];
+% lb = [120;10;0.5;12;10;1.3;0];
+% ub = [160;20;1;80;30;1.8;100];
 
-[x,resnorm,~,exitflag,output] = lsqcurvefit(F,xo,Z_1_70_n,Intensity_1_70_n,lb,ub,options)
+% CORRECTED Z
+% xo = [161;0.16;0.1;70;0.24;40;55];
+% lb = [120;0.1;-10;12;0.1;30;0];
+% ub = [220;100;10;1000;2.5;45;100];
+
+% SLICES
+xo = [161;0.16;28.37;25;0.24;44.9;55];
+lb = [50;0.1;10;12;0.1;30;0];
+ub = [220;100;30;1000;2.5;45;100];
+
+
+[x,resnorm,~,exitflag,output] = lsqcurvefit(F,xo,Z_1_57_extended,Intensity_1_57_extended,lb,ub,options)
 
 f1 = @(z) x(1)*(2*besselj(1,x(2)*(z-x(3)))./(x(2)*(z-x(3)))).^2 + x(7);
-mini = fminbnd(f1, 10,35)
+mini = fminbnd(f1, 10,60)
 
 f2 = @(z) x(4)*(2*besselj(1,x(5)*(z-x(6)))./(x(5)*((z-x(6))))).^2 + x(7);
 f2_minus = @(z) -x(4)*(2*besselj(1,x(5)*(z-x(6)))./(x(5)*((z-x(6))))).^2 + x(7);
-maxi = fminbnd(f2_minus, 10,35)
+maxi = fminbnd(f2_minus, 10,60)
 
 
-plot(Z_1_70_n,Intensity_1_70_n,'*');
+plot(Z_1_57_extended,Intensity_1_57_extended,'*');
 hold on
-plot(Z_1_70_n,F(x,Z_1_70_n),'r-.');
-plot(Z_1_70_n,f1(Z_1_70_n), 'k--');
-plot(Z_1_70_n,f2(Z_1_70_n), 'b:');
-xlabel('Z spim [slices]')
+plot(Z_1_57_extended,F(x,Z_1_57_extended),'r-.');
+plot(Z_1_57_extended,f1(Z_1_57_extended), 'k--');
+plot(Z_1_57_extended,f2(Z_1_57_extended), 'b:');
+xlabel('Z spim [um]')
 ylabel('Intensity [AU]')
-title('Intensity depth profile - Two 2um beads separated 35um - Normalized Intensity')
+title('Z-corrected intensity depth profile - Two 2um beads @0um & @28.5um - Normalized Intensity')
 legend('Recorded Intensity','Fitted - Airy Functions', 'First Airy disk', 'Second Airy Disk')
 hold off
 
 
 
-
-
-
 % x =
 % 
-%     1.4204
-%     0.1366
-%    25.3978
-%     0.5484
-%     0.0687
-%    42.2730
+%   105.0182
+%     0.2397
+%    12.7496
+%    26.0786
+%     0.1790
+%    31.6148
+%    30.2621
 % 
 % 
 % resnorm =
 % 
-%    3.4346e+03
+%    1.3410e+03
 % 
 % 
 % exitflag =
@@ -58,19 +68,20 @@ hold off
 % 
 %   struct with fields:
 % 
-%     firstorderopt: 15.7422
-%        iterations: 9
-%         funcCount: 70
+%     firstorderopt: 18.1777
+%        iterations: 16
+%         funcCount: 136
 %      cgiterations: 0
 %         algorithm: 'trust-region-reflective'
-%          stepsize: 0.0013
-
-% syms z
-% f1 = x(1)*(2*(besselj(1,x(2)*(z-x(3)))/x(2)).^2)
-% f1_prime = diff(f1);
+%          stepsize: 0.0039
+%           message: 'Local minimum possible.??lsqcurvefit stopped because the final change in the sum of squares relative to ?its initial value is less than the default value of the function tolerance.??Stopping criteria details:??Optimization stopped because the relative sum of squares (r) is changing?by less than options.FunctionTolerance = 1.000000e-06.??Optimization Metric                                  Options?relative change r =   7.66e-07             FunctionTolerance =   1e-06 (default)'
 % 
-% crit_pts1 = solve(f1_prime)
 % 
-% f2 = x(4)*(2*(besselj(1,x(5)*(z-x(6)))/x(5)).^2)
-% f2_prime = diff(f2);
-% crit_pts2 = solve(f2_prime)
+% mini =
+% 
+%    28.7366
+% 
+% 
+% maxi =
+% 
+%    31.6149
